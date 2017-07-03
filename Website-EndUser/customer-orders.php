cb675a587fa566f7d3a32288b9fc119ee0660fe6
KakaloseQ <superquan1312@gmail.com>
2017-07-03 20:35:40 +0700

    User Interface - Quan
    HTML -> PHP
    Modified Files:
    customer-account.php, customer-order.php, customer-orders.php, customer-wishlist.php, customer_menu.php
    L:404.php, blog.php, post.php, register.php

---
+++ Website-EndUser/customer-orders.php
@@ -0,0 +1,103 @@
+<?php
+  include "includes/header.php";
+  include "includes/navbar.php";
+?>
+
+    <div id="all">
+
+        <div id="content">
+            <div class="container">
+
+                <div class="col-md-12">
+
+                    <ul class="breadcrumb">
+                        <li><a href="#">Home</a>
+                        </li>
+                        <li>My orders</li>
+                    </ul>
+
+                </div>
+
+                <?php
+                  include "includes/customer_menu.php";
+                ?>
+
+                <div class="col-md-9" id="customer-orders">
+                    <div class="box">
+                        <h1>My orders</h1>
+
+                        <p class="lead">Your orders on one place.</p>
+                        <p class="text-muted">If you have any questions, please feel free to <a href="contact.php">contact us</a>, our customer service center is working for you 24/7.</p>
+
+                        <hr>
+
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Order</th>
+                                        <th>Date</th>
+                                        <th>Total</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th># 1735</th>
+                                        <td>22/06/2013</td>
+                                        <td>$ 150.00</td>
+                                        <td><span class="label label-info">Being prepared</span>
+                                        </td>
+                                        <td><a href="customer-order.php" class="btn btn-danger btn-sm">View</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th># 1735</th>
+                                        <td>22/06/2013</td>
+                                        <td>$ 150.00</td>
+                                        <td><span class="label label-info">Being prepared</span>
+                                        </td>
+                                        <td><a href="customer-order.php" class="btn btn-danger btn-sm">View</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th># 1735</th>
+                                        <td>22/06/2013</td>
+                                        <td>$ 150.00</td>
+                                        <td><span class="label label-success">Received</span>
+                                        </td>
+                                        <td><a href="customer-order.php" class="btn btn-danger btn-sm">View</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th># 1735</th>
+                                        <td>22/06/2013</td>
+                                        <td>$ 150.00</td>
+                                        <td><span class="label label-danger">Cancelled</span>
+                                        </td>
+                                        <td><a href="customer-order.php" class="btn btn-danger btn-sm">View</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th># 1735</th>
+                                        <td>22/06/2013</td>
+                                        <td>$ 150.00</td>
+                                        <td><span class="label label-warning">On hold</span>
+                                        </td>
+                                        <td><a href="customer-order.php" class="btn btn-danger btn-sm">View</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /#content -->
+<?php
+    include "includes/footer.php";
+?>
